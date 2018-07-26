392cb6ecbd4b6706240419a9684f4d874acdeb47
dembasy <dembasy1227@gmail.com>
2018-07-26 16:12:47 +0200

    done

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+  $ageM = 22 ;
+  $ageW = 15;
+  $genderM = 'men' ;
+  $genderW = 'women';
+
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice3</title>
+  </head>
+  <body>
+    <p><?php
+    if ($ageM.$genderM >= 18 ) {
+      echo 'Vous êtes un homme et vous êtes majeur';
+    }
+    else {
+      echo 'Vous êtes un homme et vous êtes mineur';
+    }
+    ?></p>
+    <p><?php
+    if ($ageW.$genderW >= 18 ) {
+      echo 'Vous êtes une femme et vous êtes majeur';
+    }
+    else {
+      echo 'Vous êtes une femme et vous êtes mineur';
+    }
+    ?></p>
+  </body>
+</html>
